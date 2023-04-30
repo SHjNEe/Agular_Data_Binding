902e21fb17600ea3597262f69516dda8d2fc780a
SHjNEe <baotrung.jpvn@gmail.com>
2023-05-01 00:29:28 +0700

    Add

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-
+  @Output('featureSelect') featureSelect = new EventEmitter<string>();
+  onSelect(feature: string): void {
+    this.featureSelect.emit(feature);
+  }
 }
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input, Output,EventEmitter } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
+  @Input() recipe: Recipe;
+
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
+    // console.log(this.recipe)
   }
 
+  onSelected() {
+    this.recipeSelected.emit(this.recipe);
+  }
 }
